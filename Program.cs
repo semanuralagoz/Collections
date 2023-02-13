67781bfd1cec7040c2c8dd532816b156814cecad
semanuralagoz <76127594+semanuralagoz@users.noreply.github.com>
2023-02-13 20:52:27 +0300

    Add files via upload
    Dizi ve liste örnekleri

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new  string[] { "Sema", "Yilmaz", "Ayse" };
+            Console.WriteLine(isimler[0]);
+            isimler = new string[4];
+            isimler[3] = "İlker";
+            Console.WriteLine(isimler[3]);
+            Console.WriteLine(isimler[1]);
+            //diziler genişletilmez, yeni eleman eklersek eski elemanları kaybederiz
+
+
+            List<string> isimler2 = new List<string> { "Lütfiye", "Seda", "Mehmet", "Eda" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[2]);
+
+            isimler2.Add("Mahmut");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            //Listlere sonradan eleman ekleyebiliriz bir sıkıntı oluşmaz 
+
+
+        }
+    }
+}
+
+
